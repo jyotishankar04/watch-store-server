d98cd629eb8f380cf7613257ceb4fe7ff4fe112f
jyotishankar04 <patrajyotishankar@gmail.com>
2024-11-12 20:05:00 +0530

    get all product with filters and get product by id : comepleted

---
+++ src/app/controllers/products.controller.ts
@@ -0,0 +1,114 @@
+import { NextFunction, Request, Response } from "express";
+import createHttpError from "http-errors";
+import prisma from "../../config/prisma.config";
+
+const getProducts = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<any> => {
+  try {
+    const { orderBy: sortBy, collection } = req.query;
+
+    // Build 'where' condition based on the collection filter
+    const where = collection
+      ? {
+          Collection: {
+            name: String(collection).toUpperCase(),
+          },
+        }
+      : {};
+
+    // Determine sorting order based on 'sortBy' value, with default as 'newest' (createdAt: "desc")
+    const orderBy: { [key: string]: "asc" | "desc" } =
+      sortBy === "atoz"
+        ? { name: "asc" }
+        : sortBy === "ztoa"
+        ? { name: "desc" }
+        : sortBy === "newest" || !sortBy // Default to newest if 'sortBy' is not provided
+        ? { createdAt: "desc" }
+        : sortBy === "oldest"
+        ? { createdAt: "asc" }
+        : sortBy === "high"
+        ? { price: "desc" }
+        : sortBy === "low"
+        ? { price: "asc" }
+        : { createdAt: "desc" };
+
+    const products = await prisma.products.findMany({
+      where: where,
+      include: {
+        TechnicalData: {
+          include: {
+            dimensions: true,
+          },
+        },
+        features: true,
+        images: true,
+        Collection: true,
+        Stocks: {
+          select: {
+            quantity: true,
+          },
+        },
+        _count: true,
+      },
+      orderBy: orderBy,
+    });
+
+    if (!products) {
+      return next(createHttpError(500, "Something went wrong"));
+    }
+
+    return res.json({
+      status: "success",
+      message: "Products retrieved successfully",
+      data: products,
+    });
+  } catch (error) {
+    return next(createHttpError(500, "Something went wrong"));
+  }
+};
+
+const getProductById = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<any> => {
+  try {
+    const { id } = req.params;
+    const product = await prisma.products.findUnique({
+      where: {
+        id: id,
+      },
+      include: {
+        TechnicalData: {
+          include: {
+            dimensions: true,
+          },
+        },
+        Stocks: {
+          select: {
+            quantity: true,
+          },
+        },
+        features: true,
+        images: true,
+        Collection: true,
+        _count: true,
+      },
+    });
+    if (!product) {
+      return next(createHttpError(404, "Product not found"));
+    }
+
+    return res.status(200).json({
+      success: true,
+      data: product,
+    });
+  } catch (error) {
+    return next(createHttpError(500, "Something went wrong"));
+  }
+};
+
+export { getProducts, getProductById };
--- src/app/routes/products.routes.ts
+++ src/app/routes/products.routes.ts
@@ -1,9 +1,12 @@
 import express from "express";
+import { authMiddleware } from "../../middlewares/auth.middleware";
+import {
+  getProductById,
+  getProducts,
+} from "../controllers/products.controller";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
+router.get("/", authMiddleware, getProducts);
+router.get("/:id", authMiddleware, getProductById);
 export default router;
